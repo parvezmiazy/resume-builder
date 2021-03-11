18a4ce47275b64410af166a56d5faf5422d059f0
Parvez Ahmed <miazyparvez@gmail.com>
2021-03-11 22:54:24 +0600

    changes

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="row">
+      
+
+        <div className="col-md-3">
+        logo
+        </div>
+        <div className="col-md-6">
+        menu
+        </div>
+        <div className="col-md-3">
+        sidebar
+        </div>
+
+     
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
